92d82c986fdcdc02e4a871f071eb6c8e78eac353
sachin801 <sachinsahara2002@gmail.com>
2021-09-11 21:08:56 +0530

    express-session-1

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session =require('express-session');
+var FileStore = require('session-file-store')(session);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -30,11 +33,19 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+app.use(session({
+  name : 'session-id',
+  secret : '12345-67890-09876-54321',
+  saveUninitialized :false,
+  resave : false,
+  store : new FileStore()
+
+}))
 
 function auth(req,res,next){
-  console.log(req.signedCookies);
-  if(!req.signedCookies.user){
+  console.log(req.session);
+  if(!req.session.user){
     var authHeader =req.headers.authorization;
     if(!authHeader){
       var err =new Error('You are unauthenticated');
@@ -46,7 +57,7 @@ function auth(req,res,next){
     var username= auth[0];
     var password= auth[1];
     if(username == "admin" && password=="password"){
-      res.cookie('user','admin',{signed : true})
+      req.session.user ='admin';
       next();
     }
     else{
@@ -57,7 +68,7 @@ function auth(req,res,next){
     }
   }
   else{
-    if(req.signedCookies.user=='admin'){
+    if(req.session.user=='admin'){
       next();
     }
     else{
