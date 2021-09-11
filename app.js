4a0d30fd16a06d3b6819bfa299f68a13cce8b2a8
sachin801 <sachinsahara2002@gmail.com>
2021-09-11 13:43:03 +0530

    cookies

--- app.js
+++ app.js
@@ -30,30 +30,42 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req,res,next){
-  console.log(req.headers);
-  var authHeader =req.headers.authorization;
-  if(!authHeader){
-    var err =new Error('You are unauthenticated');
-    res.setHeader('www-Authenticate','Basic');
-    err.status =401;
-    next(err);
-  }
-  var auth= new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':')
-  var username= auth[0];
-  var password= auth[1];
-  if(username == "admin" && password=="password"){
-    next();
+  console.log(req.signedCookies);
+  if(!req.signedCookies.user){
+    var authHeader =req.headers.authorization;
+    if(!authHeader){
+      var err =new Error('You are unauthenticated');
+      res.setHeader('www-Authenticate','Basic');
+      err.status =401;
+      next(err);
+    }
+    var auth= new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':')
+    var username= auth[0];
+    var password= auth[1];
+    if(username == "admin" && password=="password"){
+      res.cookie('user','admin',{signed : true})
+      next();
+    }
+    else{
+      var err=new Error('Wrong Username and Password');
+      res.setHeader('www-Authenticate','Basic');
+      err.status=401;
+      next(err);
+    }
   }
   else{
-    var err=new Error('Wrong Username and Password');
-    res.setHeader('www-Authenticate','Basic');
-    err.status=401;
-    next(err);
+    if(req.signedCookies.user=='admin'){
+      next();
+    }
+    else{
+      var err = new Error('You are not authentocated.');
+      err.status =401;
+      return next(err);
+    }
   }
-
 }
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
