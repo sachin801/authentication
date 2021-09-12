dd32d5b2b8292d5504174b18deefac9325fec901
sachin801 <sachinsahara2002@gmail.com>
2021-09-13 01:27:18 +0530

    passport 1

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session =require('express-session');
 var FileStore = require('session-file-store')(session);
-
+var passport=require('passport');
+var authenticate=require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -43,26 +44,20 @@ app.use(session({
 
 }))
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req,res,next){
-  console.log(req.session);
-  if(!req.session.user){
+  if(!req.user){
       var err =new Error('You are unauthenticated');
-      res.setHeader('www-Authenticate','Basic');
       err.status =401;
       next(err);
   }
   else{
-    if(req.session.user=='authenticated'){
-      next();
-    }
-    else{
-      var err = new Error('You are not authentocated.');
-      err.status =403;
-      return next(err);
-    }
+    next();
   }
 }
 app.use(auth);
